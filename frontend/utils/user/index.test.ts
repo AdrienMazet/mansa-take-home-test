826cfb295b2b3b688b3c4978736f22c0931304ed
Adrien Mazet <adrien.mazet.56@gmail.com>
2022-05-02 22:27:44 +0200

    test

---
+++ frontend/utils/user/index.test.ts
@@ -0,0 +1,29 @@
+import { getFormattedFullName, getInitials } from '.';
+
+describe('getInitials', () => {
+  it.each([
+    ['', '', ''],
+    ['John', 'John', 'JJ'],
+    ['John', 'Florence', 'FJ'],
+    ['Louis Philippe', '1er', '1L'],
+  ])(
+    'return correct initials from firstName %p and lastName %p : %p',
+    (firstName, lastName, initials) => {
+      expect(getInitials(firstName, lastName)).toBe(initials);
+    },
+  );
+});
+
+describe('getFormattedFullName', () => {
+  it.each([
+    ['', '', ' '],
+    ['John', 'John', 'JOHN John'],
+    ['John', 'Florence', 'FLORENCE John'],
+    ['Louis Philippe', '1er', '1ER Louis Philippe'],
+  ])(
+    'return correct initials from firstName %p and lastName %p : %p',
+    (firstName, lastName, initials) => {
+      expect(getFormattedFullName(firstName, lastName)).toBe(initials);
+    },
+  );
+});
