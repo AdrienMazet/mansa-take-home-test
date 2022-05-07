636ece2204cac5b6df1fbcaebb91cb0e66246be6
Adrien Mazet <adrien.mazet.56@gmail.com>
2022-05-07 22:41:20 +0200

    tests

--- frontend/cypress/integration/user_details.spec.ts
+++ frontend/cypress/integration/user_details.spec.ts
@@ -1,3 +1,5 @@
+import { getFormattedFullName, getInitials } from '../../utils/user';
+
 describe('User details', () => {
   beforeEach(() => cy.visit('/'));
   it('display an avatar', () => {
@@ -5,5 +7,23 @@ describe('User details', () => {
   });
   it('display username', () => {
     cy.get('[data-cy=Username]').should('be.visible');
+  });
+  it('display a refetch user button', () => {
+    cy.contains('Refetch user').should('be.visible');
+  });
+  it('display a button that update the user when clicked', () => {
+    const newUser = {
+      name: { first: 'Ivan', last: 'Florence' },
+    };
+    cy.intercept('https://randomuser.me/api', {
+      results: [newUser],
+    });
+    cy.contains('Refetch user').click();
+    cy.contains(
+      getFormattedFullName(newUser.name.first, newUser.name.last),
+    ).should('be.visible');
+    cy.contains(getInitials(newUser.name.first, newUser.name.last)).should(
+      'be.visible',
+    );
   });
 });
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -7,19 +7,6 @@ import { getBusiness } from 'services/business';
 import { getUser } from 'services/user';
 import styled from 'styled-components';
 
-// TODO
-// tests : e2e
-// Use of cy.get('...') on data-cy attributes
-// Normally I would have mocked the API call to retrieve user details
-// And used cy.get('...') on the expected text content (initials, fullname)
-// As the API call is done server side, I would have to mess a bit
-// with the configuration to mock it
-// Test assez léger avec Cypress car pas de mocks
-// if you had to do a second iteration, what would you add or improve? Storybook, cypress mocks
-// les tests mettent un console.error à cause testing library react hooks qui supporte pas react 18
-// avant de livrer : readme, lint, test & deploy + check coverage
-// extract main page container ?
-
 type HomeProps = {
   user: User;
   business: Business;
