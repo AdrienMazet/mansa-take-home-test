f204bd8a486d6b644ca936a31eb1a30c19a4da14
Adrien Mazet <adrien.mazet.56@gmail.com>
2022-05-07 15:33:21 +0200

    naming

--- frontend/components/layout/Main.tsx
+++ frontend/components/layout/Main.tsx
@@ -5,14 +5,14 @@ type MainProps = {
   children: ReactNode;
 };
 
-const MainContent = styled.main`
+const Container = styled.main`
   flex-grow: 1;
   padding: 0px 50px;
   background-color: ${({ theme }) => theme.colors.mainBackground};
 `;
 
 const Main: React.FC<MainProps> = ({ children }) => (
-  <MainContent>{children}</MainContent>
+  <Container>{children}</Container>
 );
 
 export default Main;
